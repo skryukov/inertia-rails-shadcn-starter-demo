99dde6fabc2fc0a775c374daffc8f6d4fae95e18
Svyatoslav Kryukov <me@skryukov.dev>
2025-03-03 22:54:40 +0300

    Enable SSR deploys
    Don't forget to add these secrets to GitHub Actions:
    SSH_PRIVATE_KEY
    KAMAL_REGISTRY_PASSWORD
    RAILS_MASTER_KEY

--- app/frontend/entrypoints/inertia.ts
+++ app/frontend/entrypoints/inertia.ts
@@ -1,6 +1,6 @@
 import { createInertiaApp } from "@inertiajs/react"
 import { type ReactNode, createElement } from "react"
-import { createRoot } from "react-dom/client"
+import { createRoot, hydrateRoot } from "react-dom/client"
 
 import { initializeTheme } from "@/hooks/use-appearance"
 import PersistentLayout from "@/layouts/persistent-layout"
@@ -39,10 +39,10 @@ void createInertiaApp({
 
   setup({ el, App, props }) {
     // Uncomment the following to enable SSR hydration:
-    // if (el.hasChildNodes()) {
-    //   hydrateRoot(el, createElement(App, props))
-    //   return
-    // }
+    if (el.hasChildNodes()) {
+      hydrateRoot(el, createElement(App, props))
+      return
+    }
     createRoot(el).render(createElement(App, props))
   },
 
