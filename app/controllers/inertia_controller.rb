7a7854a3a40d5a2d6669363b423d67e4a363bb69
Svyatoslav Kryukov <me@skryukov.dev>
2025-03-03 23:33:35 +0300

    Add demo data

--- app/controllers/inertia_controller.rb
+++ app/controllers/inertia_controller.rb
@@ -6,4 +6,12 @@ class InertiaController < ApplicationController
         user: -> { Current.user.as_json(only: %i[id name email verified created_at updated_at]) },
         session: -> { Current.session.as_json(only: %i[id]) }
       }
+
+  rescue_from ActiveRecord::ReadOnlyRecord, with: :redirect_back_with_error
+
+  private
+
+  def redirect_back_with_error(error)
+    redirect_back(fallback_location: root_path, alert: error.message)
+  end
 end
